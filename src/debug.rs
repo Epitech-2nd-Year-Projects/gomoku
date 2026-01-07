ce8a234a9cd84d1a569075fa73504f678683ecfa
Dryss <dryss.margueritte@epitech.eu>
2026-01-07 17:25:01 +0100

    feat(debug): add debug module with environment-based logging using GOMOKU_DEBUG flag

---
+++ src/debug.rs
@@ -0,0 +1,19 @@
+use std::env;
+
+pub(crate) fn is_debug_enabled() -> bool {
+    if let Ok(val) = env::var("GOMOKU_DEBUG") {
+        val == "1" || val.eq_ignore_ascii_case("true")
+    } else {
+        false
+    }
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        if crate::debug::is_debug_enabled() {
+            eprintln!("[DEBUG] {}", format!($($arg)*));
+        }
+    };
+}
+
